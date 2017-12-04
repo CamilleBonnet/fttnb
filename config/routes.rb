b8ba3a4442466c4391dd4701f044c037af6596fe
Camille Bonnet <bonnet.cam@gmail.com>
2017-12-04 17:34:35 +0100

    routes for users, flats and booking done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :flats
+
+  # root to: 'flast#welcome'
+
+  # routes for the flat model
+  resources :flats do
+    resources :bookings, only: [:create, :update]
+  end
+  get '/mybookings', to: "bookings#index"
+
+  # routes for the user model
+  get 'users/:id', to: "users#show", as: "user"
+  get 'users/profile/edit', to: "users#edit", as: "edit_user_profile"
+  patch 'users/profile', to: "users#update", as: "user_profile"
+  delete 'users', to: "users#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
