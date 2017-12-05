09428c6df93c4b8d6ae9f6230303cf02a557cc9b
lucas hamani <hamanilucas@gmail.com>
2017-12-05 11:33:03 +0100

    add has_one to registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,4 +3,27 @@ class Registration < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :user
 end
+
+
+# has_one :user
+# has_many :users
+
+# def user
+#   User.find_by(registration_id: self.id)
+# end
+
+
+# def users
+#   User.where(registration_id: self.id)
+# end
+
+# class User
+#   belongs_to :registration
+
+#   def registration
+#     Registration.find(self.registration_id)
+#   end
+# end
+
